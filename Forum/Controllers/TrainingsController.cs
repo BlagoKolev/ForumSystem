4f5752d59e7136fca827006515f659f0229f2e3f
Blagovest <blagovest.kolev@gmail.com>
2022-02-15 10:45:29 +0200

    Add some style to Home page. Create TrainingsController with empty Actions.

---
+++ Forum/Controllers/TrainingsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Forum.Controllers
+{
+    public class TrainingsController : Controller
+    {
+        public IActionResult Exercises()
+        {
+            return View();
+        }
+
+        [ActionName("Street Fitness")]
+        public IActionResult StreetFitness()
+        {
+            return View();
+        }
+    }
+}
