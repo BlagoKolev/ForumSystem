f78854b9b7ffb97086c11d01fc1ef9a4ebfff50d
BlagoKolev <blagovest.kolev@gmail.com>
2022-05-26 13:17:00 +0300

    Export all JS functions in a separate JS file in wwwroot directory.

---
+++ Forum/wwwroot/js/myCustomJs.js
@@ -0,0 +1,36 @@
+﻿
+function ShowAnswers(commentId) {
+    var x = document.getElementById("answers-" + commentId);
+    var button = document.getElementById("answers-button-" + commentId);
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        button.innerHTML = "Hide Answers";
+    } else {
+        x.style.display = "none";
+        button.innerHTML = "Show Answers";
+    }
+}
+
+
+
+function confirmDelete(commentId, isDeleteClicked) {
+
+    var confirmDeleteSpan = "confirm-delete-span-" + commentId;
+    var deleteSpan = "delete-" + commentId;
+    var showAnswersButton = 'answers-button-' + commentId;
+    var writeAnswerButton = 'write-answer-' + commentId;
+
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + showAnswersButton).hide();
+        $('#' + writeAnswerButton).hide();
+        $('#' + confirmDeleteSpan).show();
+    } else {
+        $('#' + confirmDeleteSpan).hide();
+        $('#' + showAnswersButton).show();
+        $('#' + writeAnswerButton).show();
+        $('#' + deleteSpan).show();
+    }
+}
+
